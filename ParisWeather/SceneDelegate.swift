46e9375bff9dea521a61ab8c7a0e8da653fdf79b
hamza <hamzon.jb@gmail.com>
2024-06-10 15:13:23 +0200

    Added EntityResponse

--- ParisWeather/SceneDelegate.swift
+++ ParisWeather/SceneDelegate.swift
@@ -1,41 +1,34 @@
-//
-//  SceneDelegate.swift
-//  ParisWeather
-//
-//  Created by HamZa Jerbi on 10/6/24.
-//
-
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
+    
     var window: UIWindow?
-
+    
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
-              window = UIWindow(frame: windowScene.coordinateSpace.bounds)
-              window?.windowScene = windowScene
-              window?.rootViewController = ViewController()
-              window?.makeKeyAndVisible()
+        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+        window?.windowScene = windowScene
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
     }
-
-    func sceneDidDisconnect(_ scene: UIScene) {
     
+    func sceneDidDisconnect(_ scene: UIScene) {
+        
     }
-
+    
     func sceneDidBecomeActive(_ scene: UIScene) {
-
+        
     }
-
+    
     func sceneWillResignActive(_ scene: UIScene) {
-     
+        
     }
-
+    
     func sceneWillEnterForeground(_ scene: UIScene) {
-     
+        
     }
-
+    
     func sceneDidEnterBackground(_ scene: UIScene) {
-      
+        
     }
 }
